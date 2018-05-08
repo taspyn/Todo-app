ba39fc585570404ab69348154a21a1a61d19c70a
Student <Student>
2018-05-08 12:50:27 +0300

    my todo app usind vue js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-console.log("app loaded")
-const div = document.querySelector('.container')
-console.log(div)
+var app = new Vue({
+  el: '#app',
+  data: {
+    todos:[
+    "my first todo item",
+    "my second todo item",
+    "my third todo item"
+    ],
+    item:'to do item'
+  },
+  methods: {
+  	addTodo(){
+  		this.todos.push(this.item)
+  		this.item='';
+  	}
+
+  }
+})
